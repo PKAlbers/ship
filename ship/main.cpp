a6dfabe6bd3681242b8e063b7555ef383fb2a9e9
PKAlbers <pkalbers@gmail.com>
2014-11-07 11:26:02 +0000

    Initial Commit

---
+++ ship/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ship
+//
+//  Created by Patrick Albers on 07.11.2014.
+//  Copyright (c) 2014 Patrick K. Albers. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
